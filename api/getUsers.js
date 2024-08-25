4528ef27338058c2f57e9eac64b832856c17d6f9
Yuriy_Matadorishvili <63634607+Yuriymakedonskiy@users.noreply.github.com>
2024-08-25 23:43:34 +0300

    Update getUsers.js

--- api/getUsers.js
+++ api/getUsers.js
@@ -1,4 +1,7 @@
 import { MongoClient } from 'mongodb';
+const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
+const secretKey = process.env.DATA_API_KEY;
 
 export default async function handler(req, res) {
     const client = new MongoClient(process.env.MONGODB_URI);
@@ -7,4 +10,26 @@ export default async function handler(req, res) {
     const users = database.collection('users');
     const data = await users.find({}).toArray();
     res.status(200).json(data);
+
+    const { username, password } = req.body;
+
+    try {
+        const user = await users.findOne({ username, password });
+        if (!user) {
+            return res.status(401).json({ message: 'Пользователь не найден' });
+        }
+        if (user) {
+            return res.json({ message: 'Пользователь найден' });
+        }
+
+        const isPasswordValid = await bcrypt.compare(password, user.password);
+        if (!isPasswordValid) {
+            return res.status(401).json({ message: 'Неверные данные' });
+        }
+
+        const token = jwt.sign({ userId: user._id }, secretKey, { expiresIn: '1h' });
+        res.json({ token });
+    } catch (err) {
+        res.status(500).json({ message: err.message });
+    }
 }
