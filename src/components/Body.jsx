b907ae776f339a1cee32d412d7c990d3de52c9b5
Yuriy_Matadorishvili <63634607+Yuriymakedonskiy@users.noreply.github.com>
2024-08-25 23:15:30 +0300

    Update Body.jsx

--- src/components/Body.jsx
+++ src/components/Body.jsx
@@ -52,7 +52,7 @@ const Body = () => {
     React.useEffect(() => {
       const fetchServices = async () => {
         try {
-          const response = await axios.get('api/services');
+          const response = await axios.get('/api/getData');
           setServices(response.data)
         } catch (error) {
           console.error('Error fetching the services', error)
