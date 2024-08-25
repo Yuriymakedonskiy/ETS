aad4d2422971fd23e12572a9442cd4c9fac2becb
Yuriymakedonskiy <urkabestyzhev@gmail.com>
2024-08-25 18:59:44 +0300

    vercel.json

--- api/server.js
+++ api/server.js
@@ -14,6 +14,7 @@ app.use(cors());
 app.use(express.json());
 
 const mongoURL = process.env.MONGODB_URI;
+// console.log(mongoURL)
 
 mongoose.connect(mongoURL, {
     useNewUrlParser: true,
@@ -139,6 +140,7 @@ app.post('/api/login', async (req, res) => {
     }
 });
 
+module.export = app;
 
 // app.listen(port, () => {
 //     console.log(`Server is running on http://localhost:${port}`);
