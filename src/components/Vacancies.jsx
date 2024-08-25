acab64fe78a14c5a32f8fd25b93a160b6cf76f3c
Yuriy_Matadorishvili <63634607+Yuriymakedonskiy@users.noreply.github.com>
2024-08-25 21:04:45 +0300

    Update Vacancies.jsx

--- src/components/Vacancies.jsx
+++ src/components/Vacancies.jsx
@@ -12,7 +12,7 @@ const Vacancies = () => {
   useEffect(() => {
     const fetchVacancies = async () => {
       try {
-        const response = await axios.get('https://etalontrans.vercel.app/api/vacancies');
+        const response = await axios.get('/api/vacancies');
         setVacancies(response.data)
       } catch (error) {
         console.error('Error fetching the vacancies', error)
