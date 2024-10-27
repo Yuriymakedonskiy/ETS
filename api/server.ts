92da49ee010f660c2f891f1ce1475867ce754c11
Yuriymakedonskiy <urkabestyzhev@gmail.com>
2024-10-27 15:45:37 +0300

    Revert "Обновление проекта: новые файлы и настройки Webpack"
    This reverts commit f255d1fb78c2ce85683d32fff92d09c8cdcc9b4e.

--- api/server.ts
+++ api/server.ts
@@ -7,11 +7,7 @@ const dotenv = require('dotenv');
 dotenv.config();
 
 const app = express();
-<<<<<<< HEAD
 const port = 5000;
-=======
-const port = 9000;
->>>>>>> 0508475 (Обновление проекта: новые файлы и настройки Webpack)
 const secretKey = process.env.DATA_API_KEY!;
 
 mongoose.set('strictQuery', true);
